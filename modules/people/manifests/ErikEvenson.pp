5eda4b72d75837c2231a988694abd38eb620fa62
Erik Evenson <erik.e.evenson@gmail.com>
2015-05-13 19:58:45 -0500

    Adds git config

--- modules/people/manifests/ErikEvenson.pp
+++ modules/people/manifests/ErikEvenson.pp
@@ -1,5 +1,18 @@
 class people::erikevenson {
   notify { 'class people::erikevenson declared': }
-  
+
   include chrome
+
+  # Set up git
+  exec { 'git user name':
+    command => 'git config --global user.name "Erik Evenson"',
+  }
+
+  exec { 'git user email':
+    command => 'git config --global user.email "erik.e.evenson@gmail.com"',
+  }
+
+  exec { 'git push default':
+    command => 'git config --global push.default simple',
+  }
 }
